3749db7ed215b708e12053d85482df135f2b2128
Alessandro Cannarella <alessandro.cannarella@mia-platform.eu>
2025-07-09 15:03:07 +0200

    console type changed

--- internal/controller/company_controller.go
+++ internal/controller/company_controller.go
@@ -49,8 +49,6 @@ type CompanyReconciler struct {
 func (r *CompanyReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = logf.FromContext(ctx)
 
-	// TODO(user): your logic here
-
 	return ctrl.Result{}, nil
 }
 
